b664722ec1213e24325ace415efb03cec7237f18
冬至夕雪 <94821538+ws-es@users.noreply.github.com>
2024-02-02 13:58:18 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -26,8 +26,8 @@ export default siteConfig({
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: https://laomiao.site/page-icon.png,
-  defaultPageCover: https://laomiao.site/page-conver.jpg,
+  defaultPageIcon: "https://laomiao.site/page-icon.png",
+  defaultPageCover: "https://laomiao.site/page-conver.jpg",
   defaultPageCoverPosition: 0.5,
 
   // whether or not to enable support for LQIP preview images (optional)
