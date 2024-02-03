4d8fe4d2d1dc9ef750cc76cc40a26667d4c1bd11
ws-es <xcx6838@126.com>
2024-02-03 15:24:48 +0800

    update

--- site.config.ts
+++ site.config.ts
@@ -17,9 +17,11 @@ export default siteConfig({
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  // twitter: '#',
+  // github: 'ws-es',
+  QQ: '2637726000'
+  bilibili: 'UID:687417413'
+  // linkedin: '#',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
