08106b7d690ee2b188d0b82eaf2bfbbed35051c0
ws-es <xcx6838@126.com>
2024-02-03 19:13:28 +0800

    update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -122,7 +122,7 @@ export const FooterImpl: React.FC = () => {
             target='_blank'
             rel='noopener noreferrer'
           >
-            <FaGlobe />
+            <Fabilibili />
           </a>
         )}
 
