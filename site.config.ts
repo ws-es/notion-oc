00e84a09a0222b9618decaabe34fe519a8bd51e5
ws-es <xcx6838@126.com>
2024-02-03 20:48:47 +0800

    update

--- site.config.ts
+++ site.config.ts
@@ -55,14 +55,10 @@ export default siteConfig({
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
   navigationStyle: 'default'
   // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+   navigationLinks: [
+     {
+       title: 'About',
+       pageId: '5e88340aff4b48998526e20e45b462b5'
+     }
+   ]
 })
